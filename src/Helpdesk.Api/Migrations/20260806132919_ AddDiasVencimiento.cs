fdbca2b2c98950f2ceb8c26a120644a5e6d7293e
lx1 <lusi4an@gmail.com>
2026-08-06 11:30:13 -0300

    migrations

---
+++ src/Helpdesk.Api/Migrations/20260806132919_ AddDiasVencimiento.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Helpdesk.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDiasVencimiento : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "DiasVencimiento",
+                table: "Categorias",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Categorias",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "DiasVencimiento",
+                value: null);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DiasVencimiento",
+                table: "Categorias");
+        }
+    }
+}
